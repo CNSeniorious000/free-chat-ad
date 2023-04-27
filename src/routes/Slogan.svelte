bbc3d05b4319e84290a810a6a55371be2aa3b806
CNSeniorious000 <wjj123345567@icloud.com>
2023-04-27 11:40:48 +0800

    bug fix: revert

--- src/routes/Slogan.svelte
+++ src/routes/Slogan.svelte
@@ -6,7 +6,7 @@
   {#if type === "iOS"}
     AI聊天新境界。懂你不懂，聊你想聊!
   {:else if type === "Chrome"}
-    一款值得拥有的GPT插件! 效率翻倍，读写不累!
+    <span class="sm:hidden"> 一款值得拥有的GPT插件! </span> 效率翻倍，读写不累!
   {:else}
     <span class="sm:hidden"> - </span> Edge 上全球用户最多的 ChatGPT 插件
   {/if}
